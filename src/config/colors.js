0233d1853089af36c333de609bfd397e84eed57b
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2021-04-09 13:32:10 -0300

    add project

---
+++ src/config/colors.js
@@ -0,0 +1,8 @@
+export const primaryColor = '#CF073F';
+export const primaryDarkColor = '#1a1a1d';
+
+export const successColor = '#0197f6';
+export const infoColor = '#0197f6';
+
+export const errorColor = '#f2af29';
+export const warningColor = '#f2af29';
