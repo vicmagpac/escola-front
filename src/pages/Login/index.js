bf6dfebb828ff7a91d0d5c84987e3193d1fa42c0
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2021-05-26 20:47:34 -0300

    add form login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { isEmail } from 'validator';
+import { toast } from 'react-toastify';
 
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let formErrors = false;
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('E-mail inválido');
+    }
+
+    if (formErrors) return;
+  };
+
   return (
     <Container>
       <h1>Login</h1>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Seu e-mail"
+        />
+        <input
+          type="password"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          placeholder="Sua senha"
+        />
+        <button type="submit">Acessar</button>
+      </Form>
     </Container>
   );
 }
--- src/pages/Login/styled.js
+++ src/pages/Login/styled.js
@@ -1,3 +1,17 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
-export const Paragrafo = styled.p``;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 15px;
+
+  input {
+    height: 40px;
+    font-size: 18px;
+    border: 1px solid ${colors.primaryColor};
+    padding: 0 5px;
+    box-shadow: 1px 1px 1px ${colors.primaryColor};
+    margin-top: 10px;
+  }
+`;
