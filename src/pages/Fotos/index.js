9db7097bd7417983893da262f6e730574d6209cb
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2023-01-05 10:09:32 -0300

    add change foto

--- src/pages/Fotos/index.js
+++ src/pages/Fotos/index.js
@@ -1,11 +1,76 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
 import { Container } from '../../styles/GlobalStyles';
+import Loading from '../../components/Loading';
+import { Title, Form } from './styled';
+import axios from '../../services/axios';
+import history from '../../services/history';
 
-export default function Fotos() {
+export default function Fotos({ match }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [foto, setFoto] = useState('');
+  const id = get(match, 'params.id', '');
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`/alunos/${id}`);
+        const urlFoto = get(data, 'Fotos[0].url', '');
+        setFoto(urlFoto);
+        setIsLoading(false);
+      } catch {
+        toast.error('Erro ao obter imagem');
+        setIsLoading(false);
+        history.push('/');
+      }
+    };
+
+    getData();
+  }, []);
+
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+    const fotoUrl = URL.createObjectURL(file);
+
+    setFoto(fotoUrl);
+
+    const formData = new FormData();
+    formData.append('aluno_id', id);
+    formData.append('foto', file);
+
+    try {
+      setIsLoading(true);
+      await axios.post('/fotos/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      toast.success('Foto enviada com sucesso.');
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      toast.error('Erro ao enviar foto');
+    }
+  };
+
   return (
     <Container>
-      <h1>Fotos</h1>
+      <Loading isLoading={isLoading} />
+      <Title>Fotos</Title>
+      <Form>
+        <label htmlFor="foto">
+          {foto ? <img src={foto} alt="foto" /> : 'Selecionar'}
+          <input type="file" id="foto" onChange={handleChange} />
+        </label>
+      </Form>
     </Container>
   );
 }
+
+Fotos.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
--- src/pages/Fotos/styled.js
+++ src/pages/Fotos/styled.js
@@ -1,3 +1,31 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
-export const Paragrafo = styled.p``;
+export const Title = styled.h1`
+  text-align: center;
+`;
+
+export const Form = styled.form`
+  label {
+    width: 180px;
+    height: 180px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background #eeeeee;
+    border: 5px dashed ${colors.primaryColor};
+    margin: 30px auto;
+    cursor: pointer;
+    border-radius: 50%;
+    overflow: hidden;
+  }
+
+  img {
+    width: 170px;
+    height: 170px;
+  }
+
+  input {
+    display: none;
+  }
+`;
